7990488f974d3f0fa45c84fe3d33edd3a18de3b9
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-17 23:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+let form=document.getElementById("form1");
 function getFormvalue() {
     //Write your code here
+	 let firstName=form.fname.value;
+	let lastName=form.lname.value;
+	alert(firstName+" "+lastName);
+	
+	
+	
 
 }
